fb7a7b3b8fcb8eb41493b8f1ca4f112447d85f69
Marek Rehora <rehora@Mareks-Air.home>
2023-07-30 09:05:10 +0200

    Day 2 Part 2

--- src/main.rs
+++ src/main.rs
@@ -21,49 +21,54 @@ fn day2() {
     let input = fs::read_to_string("input_2.txt").expect("Input file not found");
 
     struct Box {
-        x: u32,
-        y: u32,
-        z: u32,
+        l: u32,
+        w: u32,
+        h: u32,
     }
 
     impl fmt::Display for Box {
         fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-            write!(f, "(x:{}, y:{}, z:{})", self.x, self.y, self.z)
+            write!(f, "(x:{}, y:{}, z:{})", self.l, self.w, self.h)
+        }
+    }
+
+    impl Box {
+        fn paper_area(&self) -> u32 {
+            let first = &self.l * &self.w;
+            let second = &self.w * &self.h;
+            let third = &self.l * &self.h;
+            let smallest = cmp::min(cmp::min(first, second), cmp::min(second, third));
+            2 * first + 2 * second + 2 * third + smallest
+        }
+
+        fn ribbon_length(&self) -> u32 {
+            let mut ordered = [&self.l, &self.w, &self.h];
+            ordered.sort();
+            (ordered[0] * 2 + ordered[1] * 2) + &self.l * &self.w * &self.h
         }
     }
 
     fn from_str(box_string: &str) -> Option<Box> {
-        // println!("from_str");
         let re = Regex::new(r#"^([0-9]*)x([0-9]*)x([0-9]*)"#).unwrap();
         let (_, [x, y, z]) = re.captures(box_string).unwrap().extract();
 
         match (x.parse::<u32>(), y.parse::<u32>(), z.parse::<u32>()) {
-            (Ok(x), Ok(y), Ok(z)) => Some(Box { x, y, z }),
+            (Ok(x), Ok(y), Ok(z)) => Some(Box { l: x, w: y, h: z }),
             _ => None,
         }
     }
 
-    let boxes = input
+    let (paper_area, ribbon_length): (u32, u32) = input
         .lines()
         .flat_map(|l| from_str(l))
-        .collect::<Vec<Box>>();
-    println!("They have {} boxes", boxes.len());
-
-    let paper_area: u32 = boxes
-        .iter()
-        .map(|b| {
-            let first = b.x * b.y;
-            let second = b.y * b.z;
-            let third = b.x * b.z;
-            let smallest = cmp::min(cmp::min(first, second), cmp::min(second, third));
-
-            2 * first + 2 * second + 2 * third + smallest
-        })
-        .sum();
+        .map(|b| (b.paper_area(), b.ribbon_length()))
+        .fold((0, 0), |(acc_area, acc_ribbon), (area, ribbon)| {
+            (acc_area + area, acc_ribbon + ribbon)
+        });
 
     println!(
-        "Santa will need {} square feet of paper to wrap all the presents",
-        paper_area
+        "Santa will need {} square feet of paper and {} feets of ribbon to wrap all the presents",
+        paper_area, ribbon_length
     );
 }
 
