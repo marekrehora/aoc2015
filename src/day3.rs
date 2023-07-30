f3161fb5ba58b6770ee5fe058ffc3de4548e113c
Marek Rehora <rehora@Mareks-Air.home>
2023-07-30 13:15:53 +0200

    Day 3 Part 2

--- src/day3.rs
+++ src/day3.rs
@@ -11,54 +11,64 @@ pub fn run() {
         y: i32,
     }
 
-    let mut map = HashMap::new();
-    map.insert(Coord { x: 0, y: 0 }, 1);
+    fn houses_visited(input: &String) -> HashMap<Coord, u32> {
+        let mut map = HashMap::new(); // THere may be a way to do this without any mutations
+        map.insert(Coord { x: 0, y: 0 }, 1); // The first house is always visited
 
-    println!("number of insturcitons: {}", input.chars().count());
+        input
+            .chars()
+            .fold(Coord { x: 0, y: 0 }, |coord, c| match c {
+                '^' => {
+                    let new_coord = Coord {
+                        y: coord.y + 1,
+                        ..coord
+                    };
+                    map.entry(new_coord).and_modify(|e| *e += 1).or_insert(1);
+                    new_coord
+                }
+                '>' => {
+                    let new_coord = Coord {
+                        x: coord.x + 1,
+                        ..coord
+                    };
+                    map.entry(new_coord).and_modify(|e| *e += 1).or_insert(1);
+                    new_coord
+                }
+                '<' => {
+                    let new_coord = Coord {
+                        x: coord.x - 1,
+                        ..coord
+                    };
+                    map.entry(new_coord).and_modify(|e| *e += 1).or_insert(1);
+                    new_coord
+                }
+                'v' => {
+                    let new_coord = Coord {
+                        y: coord.y - 1,
+                        ..coord
+                    };
+                    map.entry(new_coord).and_modify(|e| *e += 1).or_insert(1);
+                    new_coord
+                }
+                _ => coord, // Cannot be reached due to filter
+            });
 
-    input
-        .chars()
-        .fold(Coord { x: 0, y: 0 }, |coord, c| match c {
-            '^' => {
-                let new_coord = Coord {
-                    y: coord.y + 1,
-                    ..coord
-                };
-                map.entry(new_coord).and_modify(|e| *e += 1).or_insert(1);
-                new_coord
-            }
-            '>' => {
-                let new_coord = Coord {
-                    x: coord.x + 1,
-                    ..coord
-                };
-                map.entry(new_coord).and_modify(|e| *e += 1).or_insert(1);
-                new_coord
-            }
-            '<' => {
-                let new_coord = Coord {
-                    x: coord.x - 1,
-                    ..coord
-                };
-                map.entry(new_coord).and_modify(|e| *e += 1).or_insert(1);
-                new_coord
-            }
-            'v' => {
-                let new_coord = Coord {
-                    y: coord.y - 1,
-                    ..coord
-                };
-                map.entry(new_coord).and_modify(|e| *e += 1).or_insert(1);
-                new_coord
-            }
-            _ => coord, // Cannot be reached due to filter
-        });
+        map
+    }
 
-    println!("{}", map.get(&Coord { x: 0, y: 0 }).unwrap());
+    // map.iter().for_each(|(k, v)| {
+    //     println!("x:{} y{}: -> {}", k.x, k.y, v);
+    // });
 
-    map.iter().for_each(|(k, v)| {
-        println!("x:{} y{}: -> {}", k.x, k.y, v);
-    });
+    println!("Santa went alone to {} houses", houses_visited(&input).len());
 
-    println!("Santa went to {} houses", map.len());
+    // Part 2
+    // This is probably not the most efficient way but whatever
+    let odd_orders = &input.char_indices().filter(|(i, _)| i % 2 != 0).map(|(_, c)| c).collect();
+    let even_orders = &input.char_indices().filter(|(i, _)| i % 2 == 0).map(|(_, c)| c).collect();
+
+    let res: HashMap<Coord, u32> = houses_visited(&odd_orders).into_iter().chain(houses_visited(&even_orders)).collect();
+
+    println!("Santa and Robo-Santa went to {} houses", res.len());
+
 }
