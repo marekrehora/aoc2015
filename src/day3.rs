ffe3c241b0494f3694f6a05fd8c4c2387963f095
Marek Rehora <rehora@Mareks-Air.home>
2023-07-30 13:17:19 +0200

    clean some comments

--- src/day3.rs
+++ src/day3.rs
@@ -50,7 +50,7 @@ pub fn run() {
                     map.entry(new_coord).and_modify(|e| *e += 1).or_insert(1);
                     new_coord
                 }
-                _ => coord, // Cannot be reached due to filter
+                _ => coord, // invalid char, no movement
             });
 
         map
